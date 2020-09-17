b6b7b61dbdfb82aac09b6e221ba5f7ddc456ffa2
bayu <bayuteguhpamuji@gmail.com>
2020-09-17 09:17:16 +0700

    [bayu] - create style

--- App.js
+++ App.js
@@ -3,8 +3,16 @@ import {View, Text, StatusBar, FlatList} from "react-native";
 
 export default class App extends Component{
   render(){
+    const style = {
+      container:{
+        flex:1
+      },
+      item:{
+        
+      },
+    }
     return (
-      <View>
+      <View style={style.container}>
         <StatusBar backgroundColor="#000000"/>
         <FlatList/>
       </View>
